caaa8745991e5b70afe10b1b27a4395924c7bcf8
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 05:34:34 +0800

    Create dynr.sh

---
+++ src/dynr.sh
@@ -0,0 +1,17 @@
+# dynr master
+
+git clone https://github.com/mhunter1/dynr.git
+cd dynr
+./configure
+make clean install
+cd ..
+rm -rf dynr
+
+# dynr arma
+
+git clone -b arma https://github.com/mhunter1/dynr.git
+cd dynr
+./configure
+make clean install
+cd ..
+rm -rf dynr
