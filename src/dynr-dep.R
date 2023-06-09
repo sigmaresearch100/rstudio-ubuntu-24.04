42cc4d942bd8df1b624eaed6b422a0c8871474a8
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 05:48:52 +0800

    Update dynr-dep.R

--- src/dynr-dep.R
+++ src/dynr-dep.R
@@ -1,11 +1,16 @@
 root <- rprojroot::is_rstudio_project
-dep <- root$find_file(
-  "src",
-  "dynr-dep.csv"
+dep <- as.vector(
+  read.csv(
+    root$find_file(
+      "src",
+      "dynr-dep.csv"
+    ),
+    head = FALSE
+  )
 )
 for (i in seq_along(dep)) {
   install.packages(
     dep[i],
-	repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/jammy/latest")
+    repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/jammy/latest")
   )
 }
